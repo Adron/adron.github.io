80f1fb177623c7ffb644d2039371e9b0a242f5df
Adron Hall <adronhall@gmail.com>
2016-06-06 21:07:33 -0700

    Changed name servers, etc

--- zone-compositecodecom.tf
+++ zone-compositecodecom.tf
@@ -5,8 +5,16 @@ resource "google_dns_managed_zone" "compositecodecom" {
 }
 
 resource "google_dns_record_set" "compositecodecom" {
-    managed_zone = "${google_dns_managed_zone.adronme.name}"
-    name = "@.${google_dns_managed_zone.adronme.dns_name}"
+    managed_zone = "${google_dns_managed_zone.compositecodecom.name}"
+    name = "@.${google_dns_managed_zone.compositecodecom.dns_name}"
+    type = "CNAME"
+    ttl = 5
+    rrdatas = ["compositecode.wordpress.com."]
+}
+
+resource "google_dns_record_set" "blogcompositecodecom" {
+    managed_zone = "${google_dns_managed_zone.compositecodecom.name}"
+    name = "blog.${google_dns_managed_zone.compositecodecom.dns_name}"
     type = "CNAME"
     ttl = 5
     rrdatas = ["blog.adron.me."]
