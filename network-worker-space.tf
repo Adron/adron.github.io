3569b6170f566627dbf763ffa609c519a70b76e4
Adron Hall <adronhall@gmail.com>
2016-06-13 21:33:51 -0700

    Adding final network bits.

--- network-worker-space.tf
+++ network-worker-space.tf
@@ -3,3 +3,9 @@ resource "google_compute_network" "worker-space" {
   auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "default-us-east1" {
+  name = "worker-space-default"
+  ip_cidr_range = "10.128.0.0/20"
+  network = "${google_compute_network.worker-space.self_link}"
+  region = "us-central1"
+}
