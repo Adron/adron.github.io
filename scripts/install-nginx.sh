cbf98615ab15f0d013f41ac6c71b90ed42ff977a
Adron Hall <adronhall@gmail.com>
2016-07-16 11:12:54 -0700

    Added packer file and installation script for Ubuntu Nginx Server.

---
+++ scripts/install-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Added the above shebang line slightly different for packer per troubleshooting tip here:
+#   https://www.packer.io/docs/provisioners/shell.html
+
+sudo apt-get update
+sudo apt-get install -y nginx
+
+sudo ufw allow http  #adds port 80
+sudo ufw allow ssh  #adds port 22
+sudo ufw allow 'Nginx HTTP'
+
+sudo ufw enable
