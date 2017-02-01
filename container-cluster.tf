eeb53728e2a8903280ca90855c009d576366f566
Adron Hall <adronhall@gmail.com>
2017-02-01 10:04:37 -0800

    Cluster is back up.

--- container-cluster.tf
+++ container-cluster.tf
@@ -1,19 +1,19 @@
-// resource "google_container_cluster" "development" {
-//   name = "development-systems"
-//   zone = "us-west1-b"
-//   initial_node_count = 3
-//
-//   master_auth {
-//     username = "firsttry"
-//     password = "willchange"
-//   }
-//
-//   node_config {
-//     oauth_scopes = [
-//       "https://www.googleapis.com/auth/compute",
-//       "https://www.googleapis.com/auth/devstorage.read_only",
-//       "https://www.googleapis.com/auth/logging.write",
-//       "https://www.googleapis.com/auth/monitoring"
-//     ]
-//   }
-// }
+resource "google_container_cluster" "development" {
+  name = "development-systems"
+  zone = "us-west1-b"
+  initial_node_count = 3
+
+  master_auth {
+    username = "firsttry"
+    password = "willchange"
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
+}
