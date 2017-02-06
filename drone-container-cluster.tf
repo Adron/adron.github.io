db0ec970e5963e7e262032890484d511bd1af094
Adron Hall <adronhall@gmail.com>
2017-02-06 12:10:44 -0800

    Cluster created.

--- drone-container-cluster.tf
+++ drone-container-cluster.tf
@@ -1,30 +1,30 @@
-// resource "google_container_cluster" "drone" {
-//   name = "drone"
-//   zone = "us-west1-b"
-//   initial_node_count = 3
-//
-//   additional_zones = [
-//     "us-west1-b"
-//   ]
-//
-//   network = "developer-space"
-//   subnetwork = "developer-space-west1"
-//
-//   master_auth {
-//     username = "firsttry"
-//     password = "willchange"
-//   }
-//
-//   node_config {
-//     oauth_scopes = [
-//       "https://www.googleapis.com/auth/compute",
-//       "https://www.googleapis.com/auth/devstorage.read_only",
-//       "https://www.googleapis.com/auth/logging.write",
-//       "https://www.googleapis.com/auth/monitoring"
-//     ]
-//     //machine_type = "f1-micro"
-//     machine_type = "g1-small"
-//     // machine_type = "n1-standard-2"
-//     // machine_type = "n1-standard-16"
-//   }
-// }
+ resource "google_container_cluster" "drone" {
+   name = "drone"
+   zone = "us-west1-b"
+   initial_node_count = 3
+
+   additional_zones = [
+     "us-west1-b"
+   ]
+
+   network = "developer-space"
+   subnetwork = "developer-space-west1"
+
+   master_auth {
+     username = "firsttry"
+     password = "willchange"
+   }
+
+   node_config {
+     oauth_scopes = [
+       "https://www.googleapis.com/auth/compute",
+       "https://www.googleapis.com/auth/devstorage.read_only",
+       "https://www.googleapis.com/auth/logging.write",
+       "https://www.googleapis.com/auth/monitoring"
+     ]
+     //machine_type = "f1-micro"
+     machine_type = "g1-small"
+     // machine_type = "n1-standard-2"
+     // machine_type = "n1-standard-16"
+   }
+ }
