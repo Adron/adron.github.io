76f21f424fcdb4aa8ed9b3baf16717ea6be82c99
Adron Hall <adronhall@gmail.com>
2017-05-28 15:04:59 -0700

    Infrastructure yup.

--- zone-adronme.tf
+++ zone-adronme.tf
@@ -19,7 +19,7 @@ resource "google_dns_record_set" "photos" {
     ttl = 5
     rrdatas = ["domains.smugmug.com."]
 }
-//
+
 //resource "google_dns_record_set" "data" {
 //    managed_zone = "${google_dns_managed_zone.adronme.name}"
 //    name = "data.${google_dns_managed_zone.adronme.dns_name}"
