593a2a6718ccbbeb3b592d45b6ab5ecb71b4fac4
Adron Hall <adronhall@gmail.com>
2017-05-28 14:55:14 -0700

    More bits.

--- zone-thrashingcodecom.tf
+++ zone-thrashingcodecom.tf
@@ -45,9 +45,73 @@ resource "google_dns_record_set" "txt_thrashingcodecom" {
   rrdatas = ["v=spf1 include:_spf.google.com ~all"]
 }
 
+resource "google_dns_record_set" "doogle_domain_key_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "google._domainkey.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "TXT"
+  ttl = 3600
+  rrdatas = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCFJLcl28yyP+o3zjfLzCvotoNcL15xdM0+7VO/SZO6gaQ+2HPpGKLFbznHMV3XJUZujODlG8Ev1jdpkfEhhD+R39VHwHxOmnkGLl7C4Kfpsb+popsefs4/X/f1w1Rb9A8Co52a45upK2dy80DfG9Cw/93slztnNO+SbLJ/uhYEhQIDAQAB"]
+}
+
 resource "google_dns_record_set" "calendar_thrashingcodecom" {
   managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
   name = "calendar.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "docs_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "docs.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "drive_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "drive.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "groups_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "groups.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "hangouts_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "hangouts.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "mail_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "mail.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "plus_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "plus.${google_dns_managed_zone.thrashingcodecom.dns_name}"
+  type = "CNAME"
+  ttl = 3600
+  rrdatas = ["ghs.googlehosted.com."]
+}
+
+resource "google_dns_record_set" "sheets_thrashingcodecom" {
+  managed_zone = "${google_dns_managed_zone.thrashingcodecom.name}"
+  name = "sheets.${google_dns_managed_zone.thrashingcodecom.dns_name}"
   type = "CNAME"
   ttl = 3600
   rrdatas = ["ghs.googlehosted.com."]
