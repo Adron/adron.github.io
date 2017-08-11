f861af43b38919efc1aa1e42b37de3db03967d5d
Adron Hall <adronhall@gmail.com>
2017-08-11 15:36:19 -0700

    temp

--- connections.tf
+++ connections.tf
@@ -6,10 +6,10 @@ provider "google" {
 }
 
 provider "azurerm" {
-  subscription_id = "${SUBSCRIPTION_ID}"
-  // client_id       = "${CLIENT_ID}"
-  // client_secret   = "${CLIENT_SECRET}"
-  // tenant_id       = "${TENANT_ID}"
+  subscription_id = "a2c16d7f-c792-4c77-9b4a-f5c646c1fa9a"
+  client_id       = "2242ef82-d757-49ca-ad3f-ec68e1ec93c9"
+  client_secret   = "1fe4ab6c-fb8a-4f76-b492-f02dca3138d4"
+  tenant_id       = "601829dc-dae5-4bee-8acd-200ea19aa336"
 }
 
 provider "aws" {
